dbe724867b51ee4a41bfe2081e07d5cabe741ea8
nare-asus <nvellela@gmail.com>
2022-09-27 21:42:31 +0930

    Adding web layer changes to person entity

---
+++ WebApplication3tierApp/Controllers/PersonController.cs
@@ -0,0 +1,59 @@
+﻿using _2DataAccessLayer.Services;
+using _3BusinessLogicLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3tierApp.Models;
+
+namespace WebApplication3tierApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class PersonController : ControllerBase   
+    {
+
+        private readonly IPersonService _personService;
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(IPersonService personService, ILogger<PersonController> logger)
+        {
+            _personService = personService;
+            _logger = logger;
+        }
+
+        [HttpGet("", Name = "GetAll")]
+        public async Task<List<PersonDto>> GetAll()
+        {
+            var result = await _personService.GetAll();
+            return result.Select(x => x.ToPersonDto()).ToList();
+        }
+
+        [HttpGet("{personId}", Name = "GetPerson")]
+        public async Task<PersonDto?> Get(int personId)
+        {
+            var result = await _personService.GetById(personId);
+            return result?.ToPersonDto();
+        }
+
+        [HttpPost, Route("")]
+        public async Task<int> Create([FromBody] PersonDto requestDto)
+        {
+            var personModel = requestDto.ToPersonModel();
+            return await _personService.CreatePerson(personModel);
+        }
+
+        [HttpPut, Route("update")]
+        public async Task<IActionResult> Update([FromBody] PersonDto requestDto)
+        {
+            await _personService.UpdatePerson(requestDto.ToPersonModel());
+            return Ok();
+        }
+
+        [HttpDelete, Route("{personId}")]
+        public async Task<IActionResult> Delete(int personId)
+        {
+            await _personService.DeletePerson(personId);
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication3tierApp/Models/PersonDto.cs
@@ -0,0 +1,38 @@
+﻿using _1CommonInfrastructure.Models;
+
+namespace WebApplication3tierApp.Models
+{
+    public class PersonDto
+    {
+        public int PersonId { get; set; }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string? PreferredName { get; set; }
+        public DateTime? DateOfBirth { get; set; }                
+    }
+
+    public static class PersonDtoMapExtensions
+    {
+        public static PersonDto ToPersonDto(this PersonModel src)
+        {
+            var dst = new PersonDto();
+            dst.PersonId = src.PersonId;
+            dst.GivenName = src.GivenName;
+            dst.FamilyName = src.FamilyName;
+            dst.PreferredName = src.PreferredName;
+            dst.DateOfBirth = src.DateOfBirth;            
+            return dst;
+        }
+
+        public static PersonModel ToPersonModel(this PersonDto src)
+        {
+            var dst = new PersonModel();
+            dst.PersonId = src.PersonId;
+            dst.GivenName = src.GivenName;
+            dst.FamilyName = src.FamilyName;
+            dst.PreferredName = src.PreferredName;
+            dst.DateOfBirth = src.DateOfBirth;            
+            return dst;
+        }
+    }
+}
