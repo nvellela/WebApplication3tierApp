ea5d15ba979cb0dae47180a94421839438b2822d
nare-asus <nvellela@gmail.com>
2022-10-01 13:29:59 +0930

    Jira-567 Adding controller api for emplyee

---
+++ WebApplication3tierApp/Controllers/EmployeeController.cs
@@ -0,0 +1,59 @@
+﻿using _2DataAccessLayer.Services;
+using _3BusinessLogicLayer.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3tierApp.Models;
+
+namespace WebApplication3tierApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class EmployeeController : ControllerBase   
+    {
+
+        private readonly IEmployeeService _EmployeeService;
+        private readonly ILogger<EmployeeController> _logger;
+
+        public EmployeeController(IEmployeeService EmployeeService, ILogger<EmployeeController> logger)
+        {
+            _EmployeeService = EmployeeService;
+            _logger = logger;
+        }
+
+        [HttpGet("", Name = "GetAllEmployees")]
+        public async Task<List<EmployeeDto>> GetAll()
+        {
+            var result = await _EmployeeService.GetAll();
+            return result.Select(x => x.ToEmployeeDto()).ToList();
+        }
+
+        [HttpGet("{EmployeeId}", Name = "GetEmployee")]
+        public async Task<EmployeeDto?> Get(int EmployeeId)
+        {
+            var result = await _EmployeeService.GetById(EmployeeId);
+            return result?.ToEmployeeDto();
+        }
+
+        [HttpPost, Route("")]
+        public async Task<int> Create([FromBody] EmployeeDto requestDto)
+        {
+            var EmployeeModel = requestDto.ToEmployeeModel();
+            return await _EmployeeService.CreateEmployee(EmployeeModel);
+        }
+
+        [HttpPut, Route("update")]
+        public async Task<IActionResult> Update([FromBody] EmployeeDto requestDto)
+        {
+            await _EmployeeService.UpdateEmployee(requestDto.ToEmployeeModel());
+            return Ok();
+        }
+
+        [HttpDelete, Route("{EmployeeId}")]
+        public async Task<IActionResult> Delete(int EmployeeId)
+        {
+            await _EmployeeService.DeleteEmployee(EmployeeId);
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication3tierApp/Models/EmployeeDto.cs
@@ -0,0 +1,30 @@
+﻿using _1CommonInfrastructure.Models;
+
+namespace WebApplication3tierApp.Models
+{
+    public class EmployeeDto
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+                        
+    }
+
+    public static class EmployeeDtoMapExtensions
+    {
+        public static EmployeeDto ToEmployeeDto(this EmployeeModel src)
+        {
+            var dst = new EmployeeDto();
+            dst.EmployeeId = src.EmployeeId;
+            dst.EmployeeName = src.EmployeeName;            
+            return dst;
+        }
+
+        public static EmployeeModel ToEmployeeModel(this EmployeeDto src)
+        {
+            var dst = new EmployeeModel();
+            dst.EmployeeId = src.EmployeeId;
+            dst.EmployeeName = src.EmployeeName;            
+            return dst;
+        }
+    }
+}
